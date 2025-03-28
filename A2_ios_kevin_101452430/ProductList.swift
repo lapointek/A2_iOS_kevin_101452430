ccc8c211eac13ccd45947160fb722fe51ccc3846
lapointek <156107790+lapointek@users.noreply.github.com>
2025-03-28 15:17:46 -0400

    added present alert

--- A2_ios_kevin_101452430/ProductList.swift
+++ A2_ios_kevin_101452430/ProductList.swift
@@ -77,4 +77,7 @@ extension ProductListViewController: UISearchBarDelegate{
     }
     
     alert.addAction(addAction)
-    alert.addAction(UIAlertAction(title: "cancel", style: .cancel))}
+    alert.addAction(UIAlertAction(title: "cancel", style: .cancel))
+    present(alert, animated: true)
+    
+}
